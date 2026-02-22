cc65bc4c0d2126fe193f9e6d379fa88cdfb84f33
ChaHaiJiLan <shi2760992374@outlook.com>
2026-02-22 23:54:00 +0800

    0.1.1放弃旧项目重新制作

---
+++ canvas-of-the-sea/src-tauri/gen/android/app/src/main/java/com/cha_hai_ji_lan/canvas_of_the_sea/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cha_hai_ji_lan.canvas_of_the_sea
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
---
+++ canvas-of-the-sea/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container">
+  </main>
+</template>
+<script setup lang="ts">
+// import { ref } from "vue";
+// import { invoke } from "@tauri-apps/api/core";
+</script>
+
+
+
+<style scoped>
+</style>
+<style>
+</style>
